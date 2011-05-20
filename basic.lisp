67bb490fb4b943eddccd7ca87cdb218b3bea9863
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-05-20 01:11:15 -0600

    Added facilities to deal with arrays and hashtables.

--- basic.lisp
+++ basic.lisp
@@ -28,7 +28,33 @@
       (cons (car list) (replace-nth (- nth 1) (cdr list) new-val))
       (cons new-val (cdr list)) ))
 
-(define-modf-expander nth 2 (expr val new-val)
-  `(replace-nth ,(second expr) ,val ,new-val))
+(define-modf-function nth 2 (new-val nth obj)
+  (replace-nth nth obj new-val) )
 
+(defun replace-nthcdr (nth list new-val)
+  (if (> nth 0)
+      (cons (car list) (replace-nthcdr (- nth 1) (cdr list) new-val))
+      new-val ))
+
+(define-modf-function nthcdr 2 (new-val nth obj)
+  (replace-nthcdr nth obj new-val) )
+
+;; @\section{Array Manipulations}
+
+;; @Arrays are inherently non-functional structures in Common Lisp.  This means
+;; that these methods will be fundamentally inefficient.  However, they are
+;; extremely convenient and, for small arrays or light usage, any inefficiency
+;; will probably go unnoticed.
+
+;; These use the Alexandria library.
+
+(define-modf-function aref 1 (new-val array &rest idx)
+  (let ((new-arr (alexandria:copy-array array)))
+    (setf (apply #'aref new-arr idx) new-val)
+    new-arr ))
+
+(define-modf-function gethash 2 (new-val key hash-table)
+  (let ((new-hash (alexandria:copy-hash-table hash-table)))
+    (setf (gethash key new-hash) new-val)
+    new-hash ))
 
