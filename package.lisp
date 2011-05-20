a4b543807c32adf3838c579e6984b2446d895efa
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-05-19 20:43:58 -0600

    Export MODF-EVAL

--- package.lisp
+++ package.lisp
@@ -2,6 +2,7 @@
 (defpackage :modf
     (:use :cl :toolbox :iter)
   (:export modf
+           modf-eval
            define-modf-rewrite
            define-modf-function
            define-modf-method
