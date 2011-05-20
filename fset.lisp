c9230b1af579fa5ce6deefc11f5e9ddc04578f4d
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-05-20 02:28:04 -0600

    Extended the fset support in MODF.

--- fset.lisp
+++ fset.lisp
@@ -3,10 +3,33 @@
 
 ;; @\section{FSet Integration}
 
-(define-modf-expander fset:lookup (expr val new-val)
-  (values
-    `(fset:with ,val ,(third expr) ,new-val)
-    #'second ))
+;; @This works with seqs and maps.
+
+(define-modf-function fset:lookup 1 (new-val collection key)
+  (fset:with collection key new-val) )
 
 (define-modf-rewrite fset:@ (expr)
   `(fset:lookup ,@(rest expr)) )
+
+(define-modf-function fset:subseq 1
+    (new-val collection start
+             &optional (end (fset:size collection)) )
+  (let ((seq (fset:convert 'fset:seq new-val)))
+    (fset:concat (fset:subseq collection 0 start)
+                 (fset:concat (fset:subseq seq 0 (- end start))
+                              (fset:subseq collection (+ start (fset:size seq))) ))))
+
+(define-modf-function fset:last 1 (new-val collection)
+  (fset:with-last (fset:less-last collection) new-val) )
+
+(define-modf-function fset:first 1 (new-val collection)
+  (fset:with-first (fset:less-first collection) new-val) )
+
+(define-modf-function fset:less-first 1 (new-val collection)
+  (fset:with-first (fset:convert 'fset:seq new-val) (fset:first collection)) )
+
+(define-modf-function fset:less-last 1 (new-val collection)
+  (fset:with-last (fset:convert 'fset:seq new-val) (fset:last collection)) )
+
+(define-modf-rewrite fset:tail (expr)
+  `(fset:less-first ,@(rest expr)) )
