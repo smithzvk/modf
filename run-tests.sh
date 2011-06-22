f164f14d03fa00e25ef6510e3b9b74a57d4acc4d
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-06-22 12:01:32 -0600

    Added a cross compatibility test shell script.  I am testing for compatibility in SBCL, CMUCL, CCL, CLISP, ECL, and ABCL (although ABCL fails due to lack of Closer-MOP).

---
+++ run-tests.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if which sbcl
+then
+    sbcl <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :supersede :direction :output)
+ (format out "sbcl: Success~%") )
+EOF
+fi
+
+# cmucl
+if which lisp
+then
+    lisp <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "cmucl: Success~%") )
+(quit)
+EOF
+ #    lisp -eval "(ql:quickload :modf-test)" -eval "(progn (modf-test:run-tests) (with-open-file (out #p\"test-resutls\" :if-exists :append :direction :output)
+ # (format out \"cmucl: Success~%\") ) (quit))"
+fi
+
+if which ccl
+then
+    ccl <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "ccl: Success~%") )
+EOF
+fi
+
+if which ccl64
+then
+    ccl64 <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "ccl64: Success~%") )
+EOF
+fi
+
+if which clisp
+then
+    clisp <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "clisp: Success~%"))
+EOF
+fi
+
+if which ecl
+then
+    ecl <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "ecl: Success~%"))
+EOF
+fi
+
+if which abcl
+then
+    abcl <<EOF
+(ql:quickload :modf-test)
+(modf-test:run-tests)
+(with-open-file (out #p"test-resutls" :if-exists :append :direction :output)
+ (format out "abcl: Success~%"))
+EOF
+fi
