9c009cd3939896996b300e5f5f515d99e1b0a993
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-08-07 19:06:21 -0500

    Extended the test script to test the case where the library is compiled and test the case where the library is loaded from previously compiled FASL files.

--- run-tests.sh
+++ run-tests.sh
@@ -78,3 +78,81 @@ then
  (format out "abcl: ~A~%" stefil:*last-test-result*))
 EOF
 fi
+
+# Now run again to test what happens if the library is loaded from FASL files
+# instead of compiled fresh
+
+echo >> test-results
+echo Testing the compiled library: >> test-results
+
+if which sbcl
+then
+    sbcl <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "sbcl: ~A~%" stefil:*last-test-result*) )
+EOF
+fi
+
+# cmucl
+if which lisp
+then
+    lisp <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "cmucl: ~A~%" stefil:*last-test-result*) )
+(quit)
+EOF
+fi
+
+if which ccl
+then
+    ccl <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "ccl: ~A~%" stefil:*last-test-result*) )
+EOF
+fi
+
+if which ccl64
+then
+    ccl64 <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "ccl64: ~A~%" stefil:*last-test-result*) )
+EOF
+fi
+
+if which clisp
+then
+    clisp <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "clisp: ~A~%" stefil:*last-test-result*))
+EOF
+fi
+
+if which ecl
+then
+    ecl <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "ecl: ~A~%" stefil:*last-test-result*))
+EOF
+fi
+
+if which abcl
+then
+    abcl <<EOF
+(ql:quickload :modf-test)
+(stefil:without-debugging (modf-test:run-tests))
+(with-open-file (out #p"test-results" :if-exists :append :direction :output)
+ (format out "abcl: ~A~%" stefil:*last-test-result*))
+EOF
+fi
